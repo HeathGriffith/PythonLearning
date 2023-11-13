03020b09a477724532006fd05cf4b5ab5ee55154
Heath Griffith <heathgriff@gmail.com>
2023-11-13 13:12:20 -0600

    completed and made initial static, inferior loop dynamic

--- BasicPythonConcepts/Dicts&Lists2.py
+++ BasicPythonConcepts/Dicts&Lists2.py
@@ -1,6 +1,6 @@
 '''This task closely simulates operations you might perform using Boto3 in an AWS environment. 
 It involves updating resource states, adding and removing resources, filtering based on specific criteria, and managing resource metadata. 
-These operations are foundational for scripting AWS infrastructure tasks in Python.'''
+These operations are foundational for scripting and understanding the logic behind AWS infrastructure tasks in Python.'''
 
 aws_resources = {
     "EC2": [
@@ -18,8 +18,29 @@ aws_resources = {
 }
 
 # (1) Update a Resource's Status: Change the status of EC2_instance_2 to "running".
+aws_resources["EC2"][1]["status"] = "running"
 # (2) Add a New Resource: Add a new EC2 instance named EC2_instance_3 with your choice of attributes.
+aws_resources["EC2"].append({"name": "EC2_instance_3", "creation_date": "2021-11-30", "status": "stopped", "region": "us-west-2"})
 # (3) Delete a Resource: Remove backup_bucket from the S3 resources.
+del aws_resources["S3"][1]
 # (4) List Resources in a Specific Region: Write a loop or a set of statements that lists all resources located in "us-east-1".
+
+'''static, inferior solution:
+for resource in aws_resources:
+    if aws_resources[resource][0]["region"] == "us-east-1":
+        print(aws_resources[resource][0]["name"])
+    if aws_resources[resource][1]["region"] == "us-east-1":
+        print(aws_resources[resource][1]["name"])'''
+
+for resource_type in aws_resources:
+    for resource in aws_resources[resource_type]:
+        if resource["region"] == "us-east-1":
+            print(resource["name"])
+
+
 # (5) Modify a Resource's Region: Change the region of inventory_db in RDS to "us-east-1".
+aws_resources["RDS"][1]["region"] = "us-east-1"
+
 # (6) Print the Updated Resources: Display the entire aws_resources dictionary in a readable format to review the changes made.
+print(aws_resources)
+
