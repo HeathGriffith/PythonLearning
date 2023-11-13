6553784a8eaddaf84bb707d039f3ea51d8a73db4
Heath Griffith <heathgriff@gmail.com>
2023-11-13 11:02:38 -0600

    complete exericise

--- BasicPythonConcepts/Dicts&Lists1.py
+++ BasicPythonConcepts/Dicts&Lists1.py
@@ -3,29 +3,27 @@
 
 favorite_fiction = {
     "British Literature": [
-        {"title": "Pride and Prejudice", "author": "Jane Austen", "year": "1813", "price": "$9.99"},
-        {"title": "Ivanhoe", "author": "Sir Walter Scott", "year": "1820", "price": "$7.99"},
+        {"title": "Pride and Prejudice", "author": "Jane Austen", "year": "1813", "price": 9.99},
+        {"title": "Ivanhoe", "author": "Sir Walter Scott", "year": "1820", "price": 7.99},
     ],
     "Historical Fiction": [
-        {"title": "Shogun", "author": "James Clavell", "year": "1975", "price": "$11.99"},
-        {"title": "King Rat", "author": "James Clavell", "year": "1962", "price": "$10.99"},
+        {"title": "Shogun", "author": "James Clavell", "year": "1975", "price": 11.99},
+        {"title": "King Rat", "author": "James Clavell", "year": "1962", "price": 10.99},
     ]
 }
 
 # (3) Update a book's price.
 
-favorite_fiction["British Literature"][1]["price"] = "$8.99"
+favorite_fiction["British Literature"][1]["price"] = 8.99
 
 # (4) Add a new book to a genre.
 
-favorite_fiction["Historical Fiction"].append({"title": "Tai-Pan", "author": "James Clavell", "year": "1966", "price": "$12.99"})
+favorite_fiction["Historical Fiction"].append({"title": "Tai-Pan", "author": "James Clavell", "year": "1966", "price": 12.99})
 
 #(5) Remove a book from a genre.
 
 del favorite_fiction["British Literature"][1]
 print(favorite_fiction)
-'''
-(6) Sort the books by price.
-(7) Print them in the format: "Title": [Title], "Author": [Author], "Year": [Year], "Price": [Price]
-'''
+
+
 
