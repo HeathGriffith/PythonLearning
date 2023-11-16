b820a65bd0052d4c8c6842729e0bfdb168262ac9
Heath Griffith <heathgriff@gmail.com>
2023-11-16 05:54:47 -0600

    Refactored create_bucket to use **kwargs for flexible parameter passing

--- 3_Arguments_and_Parameters/practice_kwargs1.py
+++ 3_Arguments_and_Parameters/practice_kwargs1.py
@@ -1,5 +1,6 @@
-import boto3
 
+import boto3
+'''
 def create_bucket():
     client = boto3.client('s3')
     response = client.create_bucket(
@@ -10,6 +11,24 @@ def create_bucket():
 
 def main():
     create_bucket()
+
+if __name__ == "__main__":
+    main()
+'''
+
+
+def create_bucket(**kwargs):
+    client = boto3.client('s3')
+    response = client.create_bucket(**kwargs)
+    print(response)
+
+kwargs = {
+    'Bucket': 'kwargs-practice-bucket83', 
+    'CreateBucketConfiguration':{'LocationConstraint': 'us-west-2'}
+    }
+
+def main():
+    create_bucket(**kwargs)
 
 if __name__ == "__main__":
     main()
